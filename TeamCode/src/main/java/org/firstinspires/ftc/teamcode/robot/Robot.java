2f80bef00c8739e36b2eaa2026b1670e5aeac0e2
jasonly <jly123@crprairie.org>
2022-09-20 17:32:44 -0500

    Added basic robot movement OpModes and hardware map.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+
+    public Robot(HardwareMap hardwareMap) {
+
+    }
+
+    public void driveFoward (double distanceInInches) {
+
+    }
+
+    public void driveBackwards (double distanceInInches){
+
+    }
+
+    public void turnLeft (double angleInDegrees){
+
+    }
+
+    public void turnRight (double angleInDegrees){
+
+    }
+}
