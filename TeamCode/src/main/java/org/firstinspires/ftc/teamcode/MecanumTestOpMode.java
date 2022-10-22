b95ce935bf72ac865c2264239a576a92e61c4b33
jasonly <jly123@crprairie.org>
2022-10-22 13:50:03 -0500

    Overwrited OpMode file with original code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTestOpMode.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MehanumDrivetrain {
+
+    private final long MILLISECONDS_PER_FORWARD_INCH = 500;
+    private final double FORWARD_POWER = 1;
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor rearLeft;
+    private DcMotor rearRight;
+
+    public MehanumDrivetrain(DcMotor frontLeft, DcMotor frontRight, DcMotor rearLeft, DcMotor rearRight) {
+        this.frontLeft = frontLeft;
+        this.frontRight = frontRight;
+        this.rearLeft = rearLeft;
+        this.rearRight = rearRight;
+    }
+
+    public void forward(double distanceInInches) throws InterruptedException {
+        manualControl(FORWARD_POWER, 0, 0);
+        wait((long)(distanceInInches * MILLISECONDS_PER_FORWARD_INCH));
+        manualControl(0,0,0);
+    }
+
+    public void turn(double angleInDegreesClockwise) {
+
+    }
+
+    public void strafe(double distanceInInches) {
+
+    }
+
+    public void manualControl(double forwardPower, double turnPower, double strafePower) {
+
+    }
+}
+
