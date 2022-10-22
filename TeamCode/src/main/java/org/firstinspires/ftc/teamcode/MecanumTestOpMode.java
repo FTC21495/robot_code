bcfca4fe05c01d771999dd52d5c5004354471876
EthanLaud12 <ethanlaud1124@gmail.com>
2022-10-22 14:19:49 -0500

    Sorted items out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTestOpMode.java
@@ -1,40 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
-public class MehanumDrivetrain {
+import org.firstinspires.ftc.teamcode.robot.Robot;
 
-    private final long MILLISECONDS_PER_FORWARD_INCH = 500;
-    private final double FORWARD_POWER = 1;
+@TeleOp(name="Mecanum Test Op", group="Linear Opmode")
 
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor rearLeft;
-    private DcMotor rearRight;
+class MecanumTestOpMode extends LinearOpMode {
 
-    public MehanumDrivetrain(DcMotor frontLeft, DcMotor frontRight, DcMotor rearLeft, DcMotor rearRight) {
-        this.frontLeft = frontLeft;
-        this.frontRight = frontRight;
-        this.rearLeft = rearLeft;
-        this.rearRight = rearRight;
-    }
+    @Override
+    public void runOpMode() throws InterruptedException {
 
-    public void forward(double distanceInInches) throws InterruptedException {
-        manualControl(FORWARD_POWER, 0, 0);
-        wait((long)(distanceInInches * MILLISECONDS_PER_FORWARD_INCH));
-        manualControl(0,0,0);
-    }
 
-    public void turn(double angleInDegreesClockwise) {
+        while (opModeIsActive()) {
 
-    }
 
-    public void strafe(double distanceInInches) {
-
-    }
-
-    public void manualControl(double forwardPower, double turnPower, double strafePower) {
-
+        }
     }
 }
 
