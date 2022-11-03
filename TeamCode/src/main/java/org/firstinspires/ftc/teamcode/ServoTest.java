98258803bdf0f240e8390038dfb08cb0914b592d
jasonly <jly123@crprairie.org>
2022-11-03 17:48:04 -0500

    Added servo opmode Set hardwaremap for servo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp (group = "TeleOp", name = "servoTest")
+public class ServoTest extends OpMode {
+    private Servo servo;
+
+    @Override
+    public void init() {
+        servo = hardwareMap.get(Servo.class, "servo");
+    }
+
+    @Override
+    public void loop() {
+        double currentPosition = servo.getPosition();
+        telemetry.addData("servoPosition", currentPosition);
+
+        if (gamepad1.dpad_up){
+            servo.setPosition(currentPosition + 0.01);
+        }
+
+        if (gamepad1.dpad_down){
+            servo.setPosition(currentPosition - 0.01);
+        }
+
+        telemetry.update();
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Claw.java
@@ -1,6 +1,18 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+
 public class Claw {
+
+
+ private Servo robotArm;
+ private TouchSensor touch;
+
 
     public Claw(){
 
@@ -12,8 +24,10 @@ public class Claw {
 
     }
     //Buttons on claw that indicate to robot that it is holding the cup.
-    public boolean senseCup(boolean holdingCup){
-    return holdingCup;
-    }
+    public boolean senseCup  (boolean holdingCup) {return holdingCup;}
+
+
+
+
 }
 
