3e738f5c30cf53f09db89ce1b91937f9f8969087
jasonly <jly123@crprairie.org>
2022-12-08 17:54:45 -0600

    Created if statement to check horitzontal target. Changed HorizontalNav to integer. Set driveTrain directions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Navigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Navigation.java
@@ -1,28 +1,60 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 
+import org.firstinspires.ftc.robotcore.internal.android.dx.util.IntSet;
+import org.firstinspires.ftc.teamcode.robot.drivetrain.MecanumDriveTrain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Navigation {
 
-    private HashMap<VerticalNavigation,Integer> tilesHorizontal;
-    private HashMap<VerticalNavigation, Integer> tilesVertical;
 
-    public Navigation(){
 
-        tilesHorizontal.put(VerticalNavigation.A, (5));
+    private HashMap< Integer, Integer> tilesHorizontal = new HashMap<>();
+    private HashMap<VerticalNavigation, Integer> tilesVertical = new HashMap<>();
+    private MecanumDriveTrain driveTrain;
+
+    public Navigation (MecanumDriveTrain driveTrain){
+
+        tilesVertical.put(VerticalNavigation.A, (120));
+        tilesVertical.put(VerticalNavigation.B, (96));
+        tilesVertical.put(VerticalNavigation.C, (72));
+        tilesVertical.put(VerticalNavigation.D, (48));
+        tilesVertical.put(VerticalNavigation.E, (24));
+
+        tilesHorizontal.put(1, (84));
+        tilesHorizontal.put(2, (60));
+        tilesHorizontal.put(3, (36));
+        tilesHorizontal.put(4, (12));
+        tilesHorizontal.put(5, (-12));
+
+        this.driveTrain = driveTrain;
+
 
     }
 
 
-    public void goToPosition(VerticalNavigation target){
+    public void goToPosition(VerticalNavigation target, Integer HorizontalTarget){
 
 
+        driveTrain.forward(tilesVertical.get(target));
+        checkHorizontalPosition(tilesHorizontal.get(HorizontalTarget));
 
+    }
 
+    public void checkHorizontalPosition (Integer HorizontalTarget){
+        if (tilesHorizontal.get(HorizontalTarget) > 0){
+            driveTrain.strafeLeft(tilesHorizontal.get(HorizontalTarget));
+        } else {
+            driveTrain.strafeRight(-tilesHorizontal.get(HorizontalTarget));
+        }
     }
 
     public void pathBrake() {
+
 
     }
 
